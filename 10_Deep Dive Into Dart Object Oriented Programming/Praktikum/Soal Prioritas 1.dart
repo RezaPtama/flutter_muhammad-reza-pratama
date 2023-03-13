b3a81df53ae81924fb191a865b8958ab2b189a39
RezaPtama <resapratama36@gmail.com>
2023-03-13 16:31:56 +0700

    Penambahan Comment

--- 10_Deep Dive Into Dart Object Oriented Programming/Praktikum/Soal Prioritas 1.dart
+++ 10_Deep Dive Into Dart Object Oriented Programming/Praktikum/Soal Prioritas 1.dart
@@ -1,42 +1,43 @@
 import 'dart:io';
 
-class BangunRuang {
-  int panjang = 0;
-  int lebar = 0;
-  int tinggi = 0;
+class BangunRuang { // membuat class BangunRuang
+  int panjang = 0; // atribut panjang
+  int lebar = 0; // atribut lebar
+  int tinggi = 0; // atribut tinggi
 
-  volume(){
+  volume(){ // membuat method volume
     print('Tidak Diketahui');
   }
 
 
 }
 
-class Kubus extends BangunRuang{
-  int sisi = 0;
-  @override
-  volume() {
-    return sisi * sisi * sisi;
+class Kubus extends BangunRuang{ // membuat class Kubus dengan mewariskan dari class BangunRuang
+  int sisi = 0; // atribut sisi
+
+  @override //override 
+  volume() { // mengambil pewarisan method volume dari class BangunRuang
+    return sisi * sisi * sisi; // rumus volume dari kubus
   }
 }
 
-class Balok extends Kubus {
-  @override
-  volume() {
-    return panjang * lebar * tinggi;
+class Balok extends BangunRuang { // membuat class Balok dengan mewariskan dari class BangunRuang
+  @override //override
+  volume() { // mengambil pewarisan method volume dari class BangunRuang
+    return panjang * lebar * tinggi; // rumus volume dari Balok
   }
 }
 
-void main(List<String> args) {
-  var bangun = Kubus();
-  bangun.sisi = 10;  
+void main() { 
+  var bangun = Kubus(); // mendeklrasikan var dengan nama bangun dengan memanggil class Kubus
+  bangun.sisi = 10; // mengubah nilai dari atribut sisi
   stdout.write('Volume Bangun Kubus ');
-  print(bangun.volume());
+  print(bangun.volume()); // menampilkan output hasil volume kubus
 
-  var bangun1 = Balok();
-  bangun1.panjang = 10;
-  bangun1.lebar = 20;
-  bangun1.tinggi = 30;
+  var bangun1 = Balok(); // mendeklrasikan var dengan nama bangun1 dengan memanggil class Balok
+  bangun1.panjang = 10; // mengubah nilai dari atribut panjang
+  bangun1.lebar = 20; // mengubah nilai dari atribut lebar
+  bangun1.tinggi = 30; // mengubah nilai dari atribut tinggi
   stdout.write('Volume Bangun Balok ');
-  print(bangun1.volume());
+  print(bangun1.volume()); // menampilkan output hasil volume kubus
 }
--- 10_Deep Dive Into Dart Object Oriented Programming/Praktikum/Soal Prioritas 2.dart
+++ 10_Deep Dive Into Dart Object Oriented Programming/Praktikum/Soal Prioritas 2.dart
@@ -1,33 +1,39 @@
-class Matematika {
-  hasil(){
+import 'dart:io';
+
+class Matematika { // membuat class Matematika
+  hasil(){ // membuat method hasil()
     print('Tidak Diketahui');
   }
 }
 
-class KelipatanPersekutuanTerkecil implements Matematika {
-  int x = 0;
-  int y = 0;
-  int z = 0;
-  @override
-  hasil() {
-  if (x > y) {
+class KelipatanPersekutuanTerkecil implements Matematika { // membuat class dengan mengimplementasikan dari class Matematika
+  int x = 0; // atribut x
+  int y = 0; // atribut y
+  int z = 0; // atribut z
+  
+  @override // override
+  hasil() { //mengambil method hasil dari class Matematika
+
+  //rumus kelipatan Persekutuan Terkecil
+  if (x > y) { 
   z = x;
   x = y;
   y = z;
  }
  for (z = y; z % x != 0 || z % y != 0; z++ );
- return z;
+ return z; // mengembalikan nilai z;
   }
 }
 
-class FaktorPersekutuanTerbesar implements Matematika {
-  int x = 0;
-  int y = 0;
-  int z = 0;
+class FaktorPersekutuanTerbesar implements Matematika { // membuat class dengan mengimplementasikan dari class Matematika
+  int x = 0; // atribut x
+  int y = 0; // atribut y
+  int z = 0; // atribut z
 
-  @override
-  hasil() {
-    // TODO: implement hasil
+  @override // override
+  hasil() { //mengambil method hasil dari class Matematika
+    
+    //rumus Faktor Persekutuan Terbesar
     if(x > y){
       z = x;
       x = y;
@@ -38,19 +44,21 @@ class FaktorPersekutuanTerbesar implements Matematika {
       y = x;
       x = z;
     }
-    return y;
+    return y; // mengembalikan nilai y
 
   }
 }
 
-void main(List<String> args) {
-  var operation = FaktorPersekutuanTerbesar();
-  operation.x = 34;
-  operation.y = 51;
-  print(operation.hasil());
+void main() {
+  var operation = FaktorPersekutuanTerbesar(); // mendeklrasikan var dengan nama operation dengan memanggil class FaktorPersekutuanTerbesar();
+  operation.x = 34; // mengubah nilai x pada class FaktorPersekututanTerbesar
+  operation.y = 51; // mengubah nilai y pada class FaktorPersekututanTerbesar
+  stdout.write('KPK dari ${operation.x} dan ${operation.x} adalah : ');
+  print(operation.hasil()); // memanggil method hasil dari class FaktorPersekutuanTerbesar
 
   var operation1 = KelipatanPersekutuanTerkecil();
-  operation1.x = 16;
-  operation1.y = 40;
-  print(operation1.hasil());
+  operation1.x = 16; // mengubah nilai x pada class KelipatanPersekutuanTerkecil
+  operation1.y = 40; // mengubah nilai y pada class KelipatanPersekutuanTerkecil
+  stdout.write('FPB dari ${operation1.x} dan ${operation1.x} adalah : ');
+  print(operation1.hasil()); // memanggil method hasil dari class KelipatanPersekutuanTerkecil
 }
